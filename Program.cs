5111d38a491b65ea3510f8d040952bf56421533f
Poul Henriksen <pohe@easj.dk>
2019-12-03 07:10:42 +0100

    Added comment Test af Git

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //test af git
             Console.WriteLine("This is a test of Git and Github - POHE");
             Console.ReadLine();
         }
